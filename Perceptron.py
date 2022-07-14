1aadca02f7d7161bc452160f671d7137f04a2c0c
minhthanh120 <minhthanh1200@hotmail.com>
2022-07-14 22:50:50 +0700

    1050

--- Perceptron.py
+++ Perceptron.py
@@ -1,17 +1,20 @@
-import ActivateFunction as af
+import numpy as np
+
+from ActivateFunction import Hardlim
 class Perceptron:
     _W = None
     _b = None
+    _a = None
     _i = 1
     def __init__(self, W = None, b = None, i = None):
-        self._b = b
         self._W = W
+        self._b = 0
         self._i = i
     def fit(self, X, y):
-        print(X.shape(0))
-
-#        for label in y:
-#            a = af.Hardlims(self._W.T @ self._p)
-#            e = y[0] - a
-#            self._W = self._W + e @ self._p.T
-#            self._b = self._b + e
+        d = X.shape[1]
+        if W is None:
+            self._W = np.zeros(d).reshape
+        for x in X:
+            n = self._W.T@x
+            self._a = Hardlim(n)
+            

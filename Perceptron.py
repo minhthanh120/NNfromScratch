624e9a4ca3f9e94dff19aad046282e3ad6b8ce28
minhthanh120 <minhthanh1200@hotmail.com>
2022-07-23 16:22:31 +0700

    1622

--- Perceptron.py
+++ Perceptron.py
@@ -10,7 +10,7 @@ class Perceptron:
     _i = 1
     _eta = None
 
-    def __init__(self, W=None, b=None, i=None, eta = 0.01):
+    def __init__(self, W=None, b=None, i=1, eta = 0.01):
         self._W = W
         self._b = 0
         self._i = i
@@ -20,16 +20,20 @@ class Perceptron:
         d = X.shape[1]
         if self._W is None:
             self._W = np.zeros(d).reshape(1, -1)
-        for p, label in zip(X, y):
-            n = self._W @ p.reshape(-1, 1) + self._b
-            self._a = Hardlim(n)
-            self.PLA_rule(p, label)
+        for _ in range(self._i):
+            for p, label in zip(X, y):
+                n = self._W @ p.reshape(-1, 1) + self._b
+                self._a = Hardlim(n)
+                self.PLA_rule(p, label)
 
     def PLA_rule(self, p, label):
         e = label - self._a
         self._W = self._W + e*p
-        self._b = self._b + e
+        self._b = self._b + es
 
     def predict(self, X):
+        if X.shape[-1] >1:
+            for x in X:
+                n = self._W @ x.reshape(-1, 1)+self._b
         n = self._W @ X.reshape(-1, 1)+self._b
         return Hardlim(n)
